11c654397bc48fc90c446e7721a9187c63d975c0
ksmithut <ksmith.ut@gmail.com>
2015-12-07 17:11:48 -0700

    adds parts to days

--- day01/index.js
+++ day01/index.js
@@ -1,21 +1,90 @@
 'use strict';
 
-var input = require('fs').readFileSync(__dirname + '/input.txt', 'utf8');
+/**
+ * --- Day 1: Not Quite Lisp ---
+ *
+ * Santa was hoping for a white Christmas, but his weather machine's "snow"
+ * function is powered by stars, and he's fresh out! To save Christmas, he needs
+ * you to collect fifty stars by December 25th.
+ *
+ * Collect stars by helping Santa solve puzzles. Two puzzles will be made
+ * available on each day in the advent calendar; the second puzzle is unlocked
+ * when you complete the first. Each puzzle grants one star. Good luck!
+ *
+ * Here's an easy puzzle to warm you up.
+ *
+ * Santa is trying to deliver presents in a large apartment building, but he
+ * can't find the right floor - the directions he got are a little confusing. He
+ * starts on the ground floor (floor 0) and then follows the instructions one
+ * character at a time.
+ *
+ * An opening parenthesis, (, means he should go up one floor, and a closing
+ * parenthesis, ), means he should go down one floor.
+ *
+ * The apartment building is very tall, and the basement is very deep; he will
+ * never find the top or bottom floors.
+ *
+ * For example:
+ *
+ * (()) and ()() both result in floor 0.
+ * ((( and (()(()( both result in floor 3.
+ * ))((((( also results in floor 3.
+ * ()) and ))( both result in floor -1 (the first basement level).
+ * ))) and )())()) both result in floor -3.
+ *
+ * To what floor do the instructions take Santa?
+ */
 
-var UP = '(';
-var DOWN = ')';
+exports.part1 = function(input) {
+  var ACTIONS = {
+    // UP
+    '(': function(val) { return val + 1; },
+    // DOWN
+    ')': function(val) { return val - 1; },
+  };
+  var curFloor = 0;
 
-var curFloor = 0;
-var basementPosition = null;
-
-for (var i = 0, len = input.length; i < len; i++) {
-  if (input[i] === UP) { curFloor++; }
-  else if (input[i] === DOWN) { curFloor--; }
-
-  if (curFloor < 0 && basementPosition === null) {
-    basementPosition = i + 1;
+  for (var i = 0, len = input.length; i < len; i++) {
+    curFloor = ACTIONS[input[i]](curFloor);
   }
-}
 
-console.log('basementPosition:', basementPosition);
-console.log('finalFloor:', curFloor);
+  return curFloor;
+};
+
+/**
+ * --- Part Two ---
+ *
+ * Now, given the same instructions, find the position of the first character
+ * that causes him to enter the basement (floor -1). The first character in the
+ * instructions has position 1, the second character has position 2, and so on.
+ *
+ * For example:
+ *
+ * ) causes him to enter the basement at character position 1.
+ * ()()) causes him to enter the basement at character position 5.
+ *
+ * What is the position of the character that causes Santa to first enter the
+ * basement?
+ */
+
+exports.part2 = function(input) {
+  var ACTIONS = {
+    // UP
+    '(': function(val) { return val + 1; },
+    // DOWN
+    ')': function(val) { return val - 1; },
+  };
+  var curFloor = 0;
+  var basementPosition = null;
+
+  for (var i = 0, len = input.length; i < len; i++) {
+    curFloor = ACTIONS[input[i]](curFloor);
+
+    if (curFloor < 0 && basementPosition === null) {
+      basementPosition = i + 1;
+    }
+  }
+
+  return basementPosition;
+};
+
--- day02/index.js
+++ day02/index.js
@@ -43,3 +43,109 @@ var total = input.split('\n').reduce(function(total, line) {
 
 console.log('wrapping paper ft^2 needed:', total.wrappingPaper);
 console.log('ribbon ft needed:', total.ribbons);
+
+/**
+ * The elves are running low on wrapping paper, and so they need to submit an
+ * order for more. They have a list of the dimensions (length l, width w, and
+ * height h) of each present, and only want to order exactly as much as they
+ * need.
+ *
+ * Fortunately, every present is a box (a perfect right rectangular prism),
+ * which makes calculating the required wrapping paper for each gift a little
+ * easier: find the surface area of the box, which is 2*l*w + 2*w*h + 2*h*l. The
+ * elves also need a little extra paper for each present: the area of the
+ * smallest side.
+ *
+ * For example:
+ *
+ * A present with dimensions 2x3x4 requires 2*6 + 2*12 + 2*8 = 52 square feet of
+ * wrapping paper plus 6 square feet of slack, for a total of 58 square feet.
+ *
+ * A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square feet
+ * of wrapping paper plus 1 square foot of slack, for a total of 43 square feet.
+ *
+ * All numbers in the elves' list are in feet. How many total square feet of
+ * wrapping paper should they order?
+ */
+
+exports.part1 = function(input) {
+  var DIMENSIONS_REGEX = /^(\d*)x(\d*)x(\d*)/;
+
+  function wrappingPaper(length, width, height) {
+    var lw = length * width;
+    var wh = width * height;
+    var hl = height * length;
+
+    var main = 2 * (lw + wh + hl);
+    var extra = [ lw, wh, hl ].sort(sortNumbers)[0];
+
+    return main + extra;
+  }
+
+  function sortNumbers(a, b) { return a - b; }
+
+  return input.split('\n').reduce(function(total, line) {
+    var pieces = line.match(DIMENSIONS_REGEX);
+
+    if (!pieces) return total;
+
+    var length = parseInt(pieces[1], 10);
+    var width = parseInt(pieces[2], 10);
+    var height = parseInt(pieces[3], 10);
+
+    return total + wrappingPaper(length, width, height);
+  }, 0);
+};
+
+/**
+ * --- Part Two ---
+ *
+ * The elves are also running low on ribbon. Ribbon is all the same width, so
+ * they only have to worry about the length they need to order, which they would
+ * again like to be exact.
+ *
+ * The ribbon required to wrap a present is the shortest distance around its
+ * sides, or the smallest perimeter of any one face. Each present also requires
+ * a bow made out of ribbon as well; the feet of ribbon required for the perfect
+ * bow is equal to the cubic feet of volume of the present. Don't ask how they
+ * tie the bow, though; they'll never tell.
+ *
+ * For example:
+ *
+ * A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap
+ * the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34
+ * feet.
+ *
+ * A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap
+ * the present plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14
+ * feet.
+ *
+ * How many total feet of ribbon should they order?
+ */
+
+exports.part2 = function(input) {
+  var DIMENSIONS_REGEX = /^(\d*)x(\d*)x(\d*)/;
+
+  function ribbons(length, width, height) {
+    return [ length, width, height ]
+      .sort(sortNumbers)
+      .slice(0, 2)
+      .reduce(function(total, length) {
+        return total + (length * 2);
+      }, length * width * height);
+  }
+
+  function sortNumbers(a, b) { return a - b; }
+
+  return input.split('\n').reduce(function(total, line) {
+    var pieces = line.match(DIMENSIONS_REGEX);
+
+    if (!pieces) return total;
+
+    var length = parseInt(pieces[1], 10);
+    var width = parseInt(pieces[2], 10);
+    var height = parseInt(pieces[3], 10);
+
+    return total + ribbons(length, width, height);
+  }, 0);
+}
