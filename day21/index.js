41e5b5fbf26fc62f9ac527b9d9dba68c8b3f40ed
ksmithut <ksmith.ut@gmail.com>
2015-12-20 22:22:57 -0700

    addss weapon stats

--- day21/index.js
+++ day21/index.js
@@ -67,6 +67,31 @@
  * What is the least amount of gold you can spend and still win the fight?
  **/
 
+const WEAPONS = [
+  { cost: 8, damage: 4, armor: 0 },
+  { cost: 10, damage: 5, armor: 0 },
+  { cost: 25, damage: 6, armor: 0 },
+  { cost: 40, damage: 7, armor: 0 },
+  { cost: 74, damage: 8, armor: 0 },
+];
+
+const ARMOR = [
+  { cost: 13, damage: 0, armor: 1 },
+  { cost: 31, damage: 0, armor: 2 },
+  { cost: 53, damage: 0, armor: 3 },
+  { cost: 75, damage: 0, armor: 4 },
+  { cost: 102, damage: 0, armor: 5 },
+];
+
+const RINGS = [
+  { cost: 25, damage: 1, armor: 0 },
+  { cost: 50, damage: 2, armor: 0 },
+  { cost: 100, damage: 3, armor: 0 },
+  { cost: 20, damage: 0, armor: 1 },
+  { cost: 40, damage: 0, armor: 2 },
+  { cost: 80, damage: 0, armor: 3 },
+];
+
 export function part1(input) {
 
 }
